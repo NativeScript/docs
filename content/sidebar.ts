97872e3b2b9d453de9f3284c9beed18fee137510
Ombuweb <tjiheron@gmail.com>
2022-11-19 22:32:28 +0100

    docs: initial content for Choosing an editor

--- content/sidebar.ts
+++ content/sidebar.ts
@@ -19,10 +19,15 @@ export default [
         text: 'Creating a new Project',
         link: '/creating-a-new-project',
       },
-      // {
-      //   text: 'Development Workflow',
-      //   link: '//#',
-      // },
+      {
+        text: 'Development Workflow',
+        items:[
+          {
+            text: 'Choosing an editor',
+            link: '/guide/development-workflow/choosing-an-editor'
+          }
+        ]
+      },
       {
         text: 'Tutorials',
         link: '/tutorials/',
