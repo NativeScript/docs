9614c824a78bdc2a99131837cac9a2fd21bb0f6e
Nandesora Tjihero <78957708+Ombuweb@users.noreply.github.com>
2023-08-13 23:09:54 +0200

    docs: initial content for Metadata in NativeScript (#33)
    Co-authored-by: Nathan Walker <walkerrunpdx@gmail.com>

--- content/sidebar.ts
+++ content/sidebar.ts
@@ -235,6 +235,10 @@ export default [
         link: '/guide/code-sharing',
       },
       {
+        text: 'Metadata',
+        link: '/guide/metadata'
+      },
+      {
         text: 'Marshalling',
         items: [
           {
