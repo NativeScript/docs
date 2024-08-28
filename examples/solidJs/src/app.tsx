71b4d901c8a55f9d1cd25ca0d49f11ef446b7156
Mel Bryan Flores <melbryanflores316@hotmail.com>
2024-08-28 23:51:25 +0800

    Flavor Update: Solid
    remove screenthree, and remove usage of globals. and collection

--- examples/solidJs/src/app.tsx
+++ examples/solidJs/src/app.tsx
@@ -1,7 +1,7 @@
-import "./globals";
+
+
 import { Route, StackRouter } from "./router";
 import { ScreenOne } from "./routes/screen-one";
-import { ScreenThree } from "./routes/screen-three";
 import { ScreenTwo } from "./routes/screen-two";
 import { Home } from '~/home'
 import { ActionBar } from '~/ui/actionbar'
@@ -12,7 +12,6 @@ const App = () => {
     <StackRouter initialRouteName="Home">
       <Route name="Home" component={Home} />
       <Route name="ActionBar" component={ActionBar} />
-      <Route name="ScreenThree" co  mponent={ScreenThree} />
     </StackRouter>
   );
 };
