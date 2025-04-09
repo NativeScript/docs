3631d7fdc11d4a4333276c03453f1c0d72a4c7cd
sadobass <sadobass@gmail.com>
2025-04-09 16:03:51 +0900

    fix: Next Link not updating issue

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,7 @@
-import Theme from "@nativescript/vitepress-theme";
-import "@nativescript/vitepress-theme/theme/style.css";
+import Theme from '@nativescript/vitepress-theme'
+import '@nativescript/vitepress-theme/theme/style.css'
 
 export default {
   ...Theme(),
-};
+  enhanceApp() {},
+}
